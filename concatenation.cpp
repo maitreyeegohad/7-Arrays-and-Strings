ddb68695d74c9af7e034fd20879897984576a605
Maitreyee Gohad <maitreyee.gohad@gmail.com>
2024-09-29 17:57:34 +0530

    Create concatenation.cpp

---
+++ concatenation.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string first_name,last_name;
+    first_name = "Maitreyee";
+    last_name = "Gohad";
+
+    cout<<first_name+last_name;
+}
+
+/*
+OUTPUT: 
+MaitreyeeGohad
+*/
