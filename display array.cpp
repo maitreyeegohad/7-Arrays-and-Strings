364172be0e510453ab782feaecfd99e870763ad3
Maitreyee Gohad <maitreyee.gohad@gmail.com>
2024-09-29 17:59:02 +0530

    Create display array.cpp

---
+++ display array.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x[6] = {1,4,7,8,3,9};
+    int i;
+    for (i=0;i<6;i++)
+    {
+        cout<<x[i]<<endl;
+    }
+}
+
+/*
+OUTPUT: 
+1
+4
+7
+8
+3
+9
+*/
