624feaa9f379e2cf1984e69a9225dcf2b92274c9
Maitreyee Gohad <maitreyee.gohad@gmail.com>
2024-09-29 18:00:04 +0530

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string str;
+    int i,flag=0;
+
+    cout<<"Enter a string: ";
+    cin>>str;
+
+    int n = str.length();
+
+    for(i=0;i<n;i++)
+    {
+        if(str[i] == str[n-i-1])
+        {
+            flag++;
+        }
+    }
+    
+    if(flag==n)
+    {
+        cout<<"Palindrome";
+    }
+    else{
+        cout<<"Not palindrome.";
+    }
+}
+
+/*
+OUTPUT: 
+Enter a string: malayalam
+Palindrome
+
+Enter a string: car
+Not palindrome.
+*/
